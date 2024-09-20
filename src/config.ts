dfccef57e4ac36ba5b5b6dd58c93ac76cd4f76d1
Yong Jie Ern <jieernyong1207@gmail.com>
2024-09-21 02:12:41 +0800

    DEBUG · 更改 zh_Tw 為 zh_TW

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from './types/config'
 export const siteConfig: SiteConfig = {
   title: 'Yong',
   subtitle: '避難小屋',
-  lang: 'zh_Tw',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  lang: 'zh_TW',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 0,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
