35c2c3a3e8947f1288268b73e546868e384907ab
Yong Jie Ern <jieernyong1207@gmail.com>
2024-09-25 05:42:46 +0800

    PROFILE · 更改名稱

--- src/config.ts
+++ src/config.ts
@@ -48,8 +48,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'Yong',
+  bio: 'Meow Lover | [About Me](https://jieern.serveblog.net/about/)',
   links: [
     {
       name: 'Twitter',
