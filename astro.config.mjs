3c95750a17b98f75fb383c34e89aad761f10b440
Yong Jie Ern <jieernyong1207@gmail.com>
2025-01-30 05:08:41 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yongjern.github.io/yong.github.io",
+  site: "https://jieern.serveblog.net/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
